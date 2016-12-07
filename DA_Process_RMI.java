15a600cc8788252e2a3e37ebc23f4cb459711d43
nopx <jollans94@googlemail.com>
2016-12-07 15:23:11 +0100

    fixed synchronize

--- DA_Process_RMI.java
+++ DA_Process_RMI.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 public interface DA_Process_RMI extends Remote{
 
-	public void receiveNotification(Message notification) throws RemoteException;
-	public void receiveProposal(Message proposal) throws RemoteException;
+		public void receiveNotification(Message notification) throws RemoteException;
+		public void receiveProposal(Message proposal) throws RemoteException;
+  public boolean isReady() throws RemoteException;
 }
