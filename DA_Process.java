d81f0f26f831691e4759e906affc47c290344bdd
nopx <jollans94@googlemail.com>
2016-12-08 12:05:08 +0100

    Changed Faulty behaviour

--- DA_Process.java
+++ DA_Process.java
@@ -196,7 +196,10 @@ public class DA_Process extends UnicastRemoteObject implements DA_Process_RMI{
 
 	public void broadcast(String type, int round, int value){
 		if(isFaulty){
-			if(Math.random()<0.5) return;
+			/*if(Math.random()<0.5){
+				System.out.println("Faulty Process "+this.number+" Not Sending Anything!");
+				return;
+			} */
 			value = (Math.random()<0.5)?0:1;
 		}
 		Message message = new Message(round,this.id, value);
